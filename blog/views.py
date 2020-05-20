083c9b9299fe44e4ab279a6ab501201c563424f9
RamonAlves1357 <ramon.alves@academico.ifpb.edu.br>
2020-05-20 20:38:02 -0300

    .

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+# from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
